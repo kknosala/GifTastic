13eaf4ac5c05a08dfe559f226d9a472dac6ad60c
KSK <kurtknosala@gmail.com>
2019-07-24 16:17:50 -0600

    most features complete

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     var apiKey = 'KpAFDpzhrqGAiuF7cxcm6XQmXZ32I0M4';
     var name = '';
     var viewCount = 0;
-    var currentImage = '';
+    var currentActor = '';
 
     //Function for displaying buttons
     function displayButtons(){
@@ -30,27 +30,21 @@ $(document).ready(function() {
 
         $('#gif-display').empty();
 
-        // name = $(this).attr('actor-name');
-        // viewCount = 10;
-
         var apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=' + apiKey + '&q=' + name + '&limit=' + viewCount;
 
         $.ajax({
             url: apiURL,
             method: "GET"
         }).then(function(response) {
-            console.log(response);
             
             for(var i = 0; i < response.data.length; i++){
 
                 var gifDiv = $('<div>')
                 var stillGif = response.data[i].images.fixed_height_still.url;
                 var motionGif = response.data[i].images.fixed_height.url;
-                console.log(motionGif);
                 var gifRating = response.data[i].rating;
 
                 gifDiv.addClass('m-2')
-
                 gifDiv.append('<p>Rated: ' + gifRating);
 
                 var gifImage = $('<img>');
@@ -72,24 +66,26 @@ $(document).ready(function() {
             
         event.preventDefault();
 
-        var actor = $('#name-search').val().trim();
+        if($('#name-search').val()){
+            var actor = $('#name-search').val().trim();
 
-        if(actorArray.indexOf(actor) === -1){
-            actorArray.push(actor);
+            if(actorArray.indexOf(actor) === -1){
+                actorArray.push(actor);
+            }
+
+            displayButtons();
+
+            name = actor;
+            currentActor = name;
+
+            viewCount = $('#how-many').val();
+            console.log('view count' + viewCount)
+            postGif();
         }
-
-        displayButtons();
-
-        name = actor;
-
-        viewCount = $('#how-many').val();
-
-        postGif();
             
     })
 
     $(document).on('click', '.Gif-Image', function() {
-        console.log('you clicked me')
         animate = $(this).attr('motion')
 
         if(animate === 'still'){
@@ -104,11 +100,23 @@ $(document).ready(function() {
     $(document).on('click', '.movie-button', function(){
         
         name = $(this).attr('actor-name');
+        currentActor = name;
         viewCount = 10;
         postGif()
     })
 
+    $(document).on('click', '#more-gifs', function(){
 
+        event.preventDefault();
+        
+        if(currentActor){
+            name = currentActor
+            console.log('view count before' + viewCount)
+            viewCount = Number($('#how-many-more').val()) + Number(viewCount);
+            console.log('view count after' + viewCount)
+            postGif()
+        }
+    })
 
 
 
